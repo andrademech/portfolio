016ace67dce4dc09fb6e1c90698861a2d99c794b
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-11-15 01:51:56 -0300

    iniciando tradução

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,17 @@ import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.jpg';
 import HeaderSocials from './HeaderSocials';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I am</h5>
+        <h5>{t('Hello, I am')}</h5>
         <h1>Herberth Andrade</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h5 className="text-light">{t('Full Stack Developer')}</h5>
         <CTA />
         <HeaderSocials />
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
 import './index.css';
+import './i18n/index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
